c9b45f22e03adcb09040c875e6d17a333ee0a5b0
Natanael Carvalho Dos Santos <santosc7mkdg@gmail.com>
2026-02-02 21:53:06 -0300

    corrigindo build

--- dist/database/index.js
+++ dist/database/index.js
@@ -2,11 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.database = void 0;
 const sequelize_1 = require("sequelize");
+const env_1 = require("../config/env");
 require("dotenv/config");
-if (!process.env.DATABASE_URL) {
-    throw new Error("DATABASE_URL não definida");
-}
-exports.database = new sequelize_1.Sequelize(process.env.DATABASE_URL, {
+exports.database = new sequelize_1.Sequelize(env_1.DATABASE_URL, {
     dialect: 'postgres',
     logging: false,
     dialectOptions: {

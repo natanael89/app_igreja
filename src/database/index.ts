0c29199bb2a153aff7b1e528ee87c8d36ea15379
Natanael Carvalho Dos Santos <santosc7mkdg@gmail.com>
2026-02-02 21:38:26 -0300

    corrigindo api

--- src/database/index.ts
+++ src/database/index.ts
@@ -2,10 +2,6 @@ import { Sequelize } from "sequelize";
 import { DATABASE_URL } from "../config/env";
 import "dotenv/config"
 
-if(!DATABASE_URL){
-   throw new Error("DATABASE_URL não definida")
-}
-
 export const database = new Sequelize(DATABASE_URL, { 
    dialect: 'postgres',
    logging: false,

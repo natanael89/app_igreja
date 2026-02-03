47a6a045aa5416a21488fe886437906baf6f7c88
Natanael Carvalho Dos Santos <santosc7mkdg@gmail.com>
2026-02-03 12:08:11 -0300

    corrigir configuração

--- src/config/env.ts
+++ src/config/env.ts
@@ -9,6 +9,6 @@ if(!process.env.DATABASE_URL){
     throw new Error("A variável DATABSE_URL está faltando no ambiente!")
 }
 
-export const DATABASE_URL = process.env.DATABASE_URL;
+export const DATABASE_URL = "postgresql://igreja_db_gtdt_user:cTDD2zjE0259SAShBU6RNInbkg6OYoi1@dpg-d5rqlcngi27c73f6rdg0-a.ohio-postgres.render.com:5432/igreja_db_gtdt;"
 export const JWT_SECRET = process.env.JWT_SECRET || "default_secret"
 export const NODE_ENV = process.env.NODE_ENV || "development"

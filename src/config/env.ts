ced690a06869e517f26c14c868d254a6e8d1a267
Natanael Carvalho Dos Santos <santosc7mkdg@gmail.com>
2026-02-02 21:28:37 -0300

    corrigindo arquivo

--- src/config/env.ts
+++ src/config/env.ts
@@ -2,6 +2,6 @@ import dotenv from "dotenv";
 
 dotenv.config()
 
-export const DATABASE_URL = process.env.DATABASE_URL || ""
+export const DATABASEL_URL = process.env.DATABASEL_URL || ""
 export const JWT_SECRET = process.env.JWT_SECRET || ""
 export const NODE_ENV = process.env.NODE_ENV || "development"
--- src/database/index.ts
+++ src/database/index.ts
@@ -1,11 +1,11 @@
 import { Sequelize } from "sequelize";
 import "dotenv/config"
 
-if(!process.env.DATABASE_URL){
+if(!process.env.DATABASEL_URL){
    throw new Error("DATABASEL_URL não definida")
 }
 
-export const database = new Sequelize(process.env.DATABASE_URL, { 
+export const database = new Sequelize(process.env.DATABASEL_URL, { 
    dialect: 'postgres',
    logging: false,
    dialectOptions: {

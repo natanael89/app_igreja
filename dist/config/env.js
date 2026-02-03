f3227d4015ff8bd899aebc3f1e7c27da50df946f
Natanael Carvalho Dos Santos <santosc7mkdg@gmail.com>
2026-02-02 21:19:15 -0300

    add pasta config na api

---
+++ dist/config/env.js
@@ -0,0 +1,12 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.NODE_ENV = exports.JWT_SECRET = exports.DATABASE_URL = void 0;
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+exports.DATABASE_URL = process.env.DATABASE_URL || "";
+exports.JWT_SECRET = process.env.JWT_SECRET || "";
+exports.NODE_ENV = process.env.NODE_ENV || "development";
+//# sourceMappingURL=env.js.map

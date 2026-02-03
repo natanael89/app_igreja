becd858b016ca4db95145aa3c7c094df1babdfcc
Natanael Carvalho Dos Santos <santosc7mkdg@gmail.com>
2026-02-02 21:13:49 -0300

    restartando pasta dist

--- dist/services/jwt.Service.js
+++ dist/services/jwt.Service.js
@@ -5,11 +5,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.jwtService = void 0;
 const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
+const env_1 = require("../config/env");
 require("dotenv/config");
 const secret = String(process.env.JWT_SECRET);
 exports.jwtService = {
     signToken: (payload, expiration) => {
-        return jsonwebtoken_1.default.sign(payload, secret, { expiresIn: expiration });
+        return jsonwebtoken_1.default.sign(payload, env_1.JWT_SECRET, { expiresIn: expiration });
     },
     verifyToken(token) {
         return jsonwebtoken_1.default.verify(token, process.env.JWT_SECRET);

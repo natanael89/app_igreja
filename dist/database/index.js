1ccca13966fea254889bcb90c600801783d8ee81
Natanael Carvalho Dos Santos <santosc7mkdg@gmail.com>
2026-02-02 23:09:22 -0300

    corrigindo novamente build

--- dist/database/index.js
+++ dist/database/index.js
@@ -6,13 +6,11 @@ const env_1 = require("../config/env");
 exports.database = new sequelize_1.Sequelize(env_1.DATABASE_URL, {
     dialect: 'postgres',
     logging: false,
-    dialectOptions: env_1.NODE_ENV === 'production'
-        ? {
-            ssl: {
-                require: true,
-                rejectUnauthorized: false,
-            },
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false,
         }
-        : {}
+    }
 });
 //# sourceMappingURL=index.js.map
--- src/database/index.ts
+++ src/database/index.ts
@@ -1,17 +1,14 @@
 import { Sequelize } from "sequelize";
 import { DATABASE_URL, NODE_ENV } from "../config/env";
 
+
 export const database = new Sequelize(DATABASE_URL, { 
    dialect: 'postgres',
    logging: false,
-   dialectOptions: 
-      NODE_ENV === 'production'
-      ? {
-          ssl: {
-            require: true,
-            rejectUnauthorized: false,
-          },
+   dialectOptions: {
+      ssl: {
+         require: true,
+         rejectUnauthorized: false,
       }
-      : {}
-   
+   }
 })
